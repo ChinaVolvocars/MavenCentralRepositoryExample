c786f1fec94407f4f63b5151ea59069be05109fe
Atlantis <AtlantisSpeed@gmail.com>
2025-11-15 19:03:43 +0800

    Hello.kt

---
+++ library/src/main/java/io/github/chinavolvocars/example/library/Hello.kt
@@ -0,0 +1,13 @@
+package io.github.chinavolvocars.example.library
+
+/**
+ * Hello
+ *
+ * @Author: mac
+ * @Date: 2025/11/15 19:02
+ */
+object Hello {
+  fun printer() {
+    print("hello")
+  }
+}
